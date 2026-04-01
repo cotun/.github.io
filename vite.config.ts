551617f87af6b7ffe4ded70cad15cafd6b68edd2
cotun <93699899+cotun@users.noreply.github.com>
2026-04-01 14:05:25 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/safe/', // 깃허브 페이지 저장소명에 맞게 설정
+  base: '/safekas/', // 깃허브 페이지 저장소명에 맞게 설정
   build: {
     outDir: 'docs',
   }
